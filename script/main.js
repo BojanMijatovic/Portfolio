8bcd0a7d677b16e8c050ff0c4a89b4c28966b039
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-01-31 20:22:37 +0100

    add files

---
+++ script/main.js
@@ -0,0 +1,6 @@
+// hide preloader
+
+window.addEventListener('load', () => {
+    document.querySelector('.preloader').classList.add('hidePreloader');
+    document.body.style.overflow = 'visible';
+})
