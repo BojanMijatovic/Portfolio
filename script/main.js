6dab3550960df1760723a0e2ee922a735a9a2f50
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-02-01 20:39:54 +0100

    work

--- script/main.js
+++ script/main.js
@@ -27,3 +27,28 @@ typewriter.typeString('I\'m Bojan Mijatovic')
     .typeString('I\'m Frontend Developer')
     .pauseFor(2500)
     .start()
+
+
+
+//  stop transition
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add('resize-transition-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-transition-stopper');
+    }, 400);
+})
+
+
+//  show navbar after scrolling
+window.addEventListener('scroll', () => {
+    let position = window.pageYOffset || document.documentElement.scrollTop ||
+        document.body.scrollTop || 0;
+    const navbar = document.querySelector('.navbar');
+    if (position > 800) {
+        navbar.style.display = 'block';
+    } else {
+        navbar.style.display = 'none';
+    }
+})
